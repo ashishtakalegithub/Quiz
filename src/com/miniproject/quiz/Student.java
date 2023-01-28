afc53fdb4a6010909e83a9e1f0e75eadc0641e87
bhagwat <bshirse1@gmail.com>
2023-01-28 11:10:29 +0530

    student details added

--- src/com/miniproject/quiz/Student.java
+++ src/com/miniproject/quiz/Student.java
@@ -1,26 +1,64 @@
 package com.miniproject.quiz;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 //POJO
 public class Student {
 	private String fName;
 	private String lName;
 	private long mobileNumber;
+	private ArrayList<String> questions;
+	private ArrayList<String> queSeq;
+	private ArrayList<String> ansSeq;
+	private TreeMap<String, String> queAndans;
+
+	public TreeMap<String, String> getQueAndans() {
+		return queAndans;
+	}
+
+	public void setQueAndans(TreeMap<String, String> queAndans) {
+		this.queAndans = queAndans;
+	}
+
+	public ArrayList<String> getQueSeq() {
+		return queSeq;
+	}
+
+	public void setQueSeq(ArrayList<String> queSeq) {
+		this.queSeq = queSeq;
+	}
+
+	public ArrayList<String> getAnsSeq() {
+		return ansSeq;
+	}
+
+	public void setAnsSeq(ArrayList<String> ansSeq) {
+		this.ansSeq = ansSeq;
+	}
+
+	public ArrayList<String> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(ArrayList<String> questions) {
+		this.questions = questions;
+	}
 
 	public String getfName() {
 		return fName;
 	}
 
 	public void setfName(String fName) {
-		this.fName = fName;
+		this.fName = fName.toUpperCase().trim();
 	}
 
 	public String getlName() {
-		
 		return lName;
 	}
 
 	public void setlName(String lName) {
-		this.lName = lName;
+		this.lName = lName.toUpperCase().trim();
 	}
 
 	public long getMobileNumber() {
@@ -31,3 +69,4 @@ public class Student {
 		this.mobileNumber = mobileNumber;
 	}
 }
+
